89ca7f441f833743f438631cd66556f89cb1c31a
unknown <yr375003@gmail.com>
2026-02-08 08:59:55 +0530

    removed helmet to fix build

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navigation from './Components/Navigation'
 import HomePage from './Pages/HomePage'
-import { HelmetProvider } from 'react-helmet-async'
 import Footer from './Components/Footer'
 
 const App = () => {
   return (
     <div>
-      <HelmetProvider>
-        <Navigation />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-        </Routes>
-        <Footer/>
-      </HelmetProvider>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+      <Footer />
     </div>
   )
 }
